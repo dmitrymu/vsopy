b90ea98ea5bc2d4ba214af72a96954dde88d4bf0
dmitrymu <d.v.mukhortov@gmail.com>
2025-03-16 19:04:36 -0700

    Modify mwasure_photometry, add unit test

--- src/vso/phot/measure.py
+++ src/vso/phot/measure.py
@@ -14,7 +14,7 @@ from photutils.aperture import (ApertureStats,
                                 SkyCircularAnnulus)
 
 
-def measure_photometry(image, stars, r_ap, r_ann):
+def measure_photometry(image, stars, r_ap, r_ann, extended=False):
     """Extract aperture photometry data from the image.
 
     Given the calibrated image and the list of star centroids in sky coordinates,
@@ -22,16 +22,21 @@ def measure_photometry(image, stars, r_ap, r_ann):
     see Warner B.D., A Practical Guide to Lightcurve Photometry and Analysis, 4.2.
 
     Flux counting is performed by photutils package
-    (https://photutils.readthedocs.io/en/stable/aperture.html). Uncertainty from
-    the image is propagated to the flux values. Flux is normalized by the image
-    exposure to electrons per second.
+    (https://photutils.readthedocs.io). Uncertainty fromthe image is propagated to
+    the flux values. Flux is normalized by the image exposure to electrons per second.
 
     The flux for central aperture F_c contains both star and sky electrons, the flux
     for annulus F_a - sky electrons only. Number of sky electrons in the central
-    aperture is.
+    aperture is
 
     F_{sky} = F_{sky mean} * (pixel count for central aperture)
+
+    Then star flux in electons is
+
     F_{star} = F_c - F_{sky}
+
+    and signal to noise ratio is
+
     SNR = F_{star} / \\sqrt{F_star + 2 * F_sky}
 
     To convert flux to magnitude, an arbitrary scale value F_0 = 1e6 e/s is used:
@@ -39,11 +44,11 @@ def measure_photometry(image, stars, r_ap, r_ann):
     M = -2.5 * log_{10}(F_{star} / F_0)
 
     Args:
-        image (CCDData): calibrated image, pixel counts in electrons.
-        stars (table-like): list of stars to be measured: auid, centroid, optional name
-        r (_type_): radius of thecircular aperture
-        r_in (_type_): inner radius of the annulus
-        r_out (_type_): outer radius of the annulus
+        image (CCDData):            calibrated image, pixel counts in electrons.
+        stars (table-like):         list of stars to be measured: AUID, centroid, optional name
+        r (Quantity):               radius of the circular aperture
+        r_ann (Quantity, Quantity): inner and outer radii of the annulus
+        extended (bool):            return additional stats (FWHM, ellipticity etc)
 
     Returns:
         QTable: photometry results:
@@ -55,17 +60,17 @@ def measure_photometry(image, stars, r_ap, r_ann):
                 relative peak (max count / saturation level)
 
     """
-    result = QTable(stars['auid', 'sky_centroid'])
+    result = QTable(stars['auid', 'radec2000'])
     if 'name' in stars.colnames:
         result['name'] = stars['name']
 
     corrected = CCDData(image)
 
-    apr = SkyCircularAperture(stars['sky_centroid'], r=r_ap)
+    apr = SkyCircularAperture(stars['radec2000'], r=r_ap)
     ap_stats = ApertureStats(corrected, apr)
 
     r_in, r_out = r_ann
-    ann = SkyCircularAnnulus(stars['sky_centroid'],
+    ann = SkyCircularAnnulus(stars['radec2000'],
                              r_in=r_in,
                              r_out=r_out)
     ann_stats = ApertureStats(corrected, ann)
@@ -93,5 +98,11 @@ def measure_photometry(image, stars, r_ap, r_ann):
                          dtype=[('mag','f4'), ('err', 'f4')])
 
     result['peak'] = np.nan if not camera else ap_stats.max.value / camera.max_adu.value
+    result['sky_centroid'] = ap_stats.sky_centroid
+
+    if extended:
+        result['ellipticity'] = ap_stats.ellipticity
+        result['fwhm'] = ap_stats.fwhm
+        result['orientation'] = ap_stats.orientation
 
     return result
