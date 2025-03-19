e2ce6add34dae3405a4c1c48a73b7388c0d9ecf4
dmitrymu <d.v.mukhortov@gmail.com>
2025-03-18 20:31:30 -0700

    Rework list_images

--- src/pipeline/list_images.py
+++ src/pipeline/list_images.py
@@ -3,13 +3,14 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.join(
     os.path.dirname(__file__), '..')))
 
-import sys
 import argparse
+import astropy.units as u
 import ccdproc as ccdp
+import numpy as np
 
 from astropy.table import QTable
+from astropy.time import Time
 from vso import util
-from pathlib import Path
 
 def parse_args():
     parser = argparse.ArgumentParser(
@@ -31,17 +32,25 @@ def main():
     work_layout = util.WorkLayout(args.work_dir)
     img_layout = util.ImageLayout(args.image_dir)
 
-    KEYS = set(['file', 'exptime', 'ccd-temp', 'filter', 'airmass', 'date-obs', 'gain'])
+    KEYS = set(['file', 'exptime', 'ccd-temp', 'filter', 'airmass', 'date-obs'])
 
     files = QTable([dict({key: row[key]
                           for key in row.colnames
-                          if key in KEYS}, dir=str(d))
+                          if key in KEYS},
+                         dir=d)
                     for d in img_layout.get_images(session).lights_dir.iterdir()
                     if d.is_dir()
                     for row in ccdp.ImageFileCollection(d).summary])
     files['image_id'] = [n+1 for n in range(len(files))]
+    files['time'] = Time(files['date-obs'])
+    files['exposure'] = files['exptime'] * u.second
+    files['temperature'] = files['ccd-temp'] * u.deg_C
+    files.add_column([str(row['dir'] / row['file']) for row in files], name='path')
 
-    files.write(work_layout.get_session(session).images_file_path,
+    images = files['image_id', 'filter', 'time', 'exposure', 'airmass', 'temperature', 'path']
+    images.meta = {'object': args.object, 'start': np.min(images['time']), 'finish': np.max(images['time'])}
+
+    images.write(work_layout.get_session(session).images_file_path,
                 format='ascii.ecsv', overwrite=args.overwrite)
 
     return 0
