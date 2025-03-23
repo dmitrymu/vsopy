031e7e9d242bbba63b379af3140ef105a2efc4f4
dmitrymu <d.v.mukhortov@gmail.com>
2025-03-22 19:10:55 -0700

    parallelize photometry measurement

--- src/pipeline/measure_images.py
+++ src/pipeline/measure_images.py
@@ -5,6 +5,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(
 
 import sys
 import argparse
+import concurrent.futures as cf
 from astropy.table import QTable, vstack
 from vso import phot
 from vso import reduce
@@ -17,9 +18,32 @@ def parse_args():
     parser.add_argument('-O', '--object', type=str, required=True, help='Object name')
     parser.add_argument('-t', '--tag', type=str, required=True, help='Tag (date)')
     parser.add_argument('-w', '--work-dir', type=str, required=True, help='Work directory')
+    parser.add_argument('-p', '--parallel', type=int, default=1, help='Number of parallel workers')
     parser.add_argument('--overwrite', action='store_true', default=False, help='Overwrite output files')
 
     return parser.parse_args()
+
+CONTEXT = None
+
+def make_context(args):
+    print('making context')
+    session = util.Session(tag=args.tag, name=args.object)
+    work_layout = util.WorkLayout(args.work_dir)
+    session_layout = work_layout.get_session(session)
+    solver = lambda path: reduce.astap_solver(path, session_layout.solved_dir)
+    matcher = reduce.CalibrationMatcher(work_layout.calibr_dir)
+    centroids = QTable.read(session_layout.centroid_file_path)
+    settings = util.Settings(session_layout.settings_file_path)
+    global CONTEXT
+    CONTEXT = (matcher, solver, centroids, settings.aperture)
+
+def measure_image(id, path):
+    print(f'measure {path}')
+    global CONTEXT
+    matcher, solver, centroids, aperture = CONTEXT
+    result = phot.process_image(path, matcher, solver, centroids, aperture)
+    result['image_id'] = id
+    return result['image_id', 'auid', 'M', 'flux', 'snr', 'peak']
 
 def main():
     args = parse_args()
@@ -28,35 +52,35 @@ def main():
     work_layout = util.WorkLayout(args.work_dir)
     session_layout = work_layout.get_session(session)
 
-    solver = lambda path: reduce.astap_solver(path, session_layout.solved_dir)
-    matcher = reduce.CalibrationMatcher(work_layout.calibr_dir)
-    centroids = QTable.read(session_layout.centroid_file_path)
-    settings = util.Settings(session_layout.settings_file_path)
-
     images = QTable.read(session_layout.images_file_path)
 
     blacklist = util.Blacklist(session_layout.blacklist_file_path)
 
-    def measure_image(id, path):
+    with cf.ProcessPoolExecutor(initializer=make_context,
+                                    initargs=(args,),
+                                    max_workers=args.parallel) as executor:
+        futures = [(image['path'], executor.submit(measure_image, image['image_id'], image['path']))
+                  for image in images if not blacklist.contains(image['path'])]
+
+    def get_result(image_result):
+        path = None
         try:
-            result = phot.process_image(path, matcher, solver, centroids, settings.aperture)
-            result['image_id'] = id
-            return result['image_id', 'auid', 'M', 'flux', 'snr', 'peak']
+            path, future = image_result
+            return future.result()
         except Exception as e:
             blacklist.add(path, e)
             return None
 
-    per_image = [measure_image(image['image_id'], image['path'])
-                 for image in images if not blacklist.contains(image['path'])]
+    tables = [get_result(f) for f in futures]
 
-    result = vstack([image for image in per_image if image is not None])
+    result = vstack([t for t in tables if t is not None])
 
     result.write(session_layout.measured_file_path, format='ascii.ecsv', overwrite=args.overwrite)
     blacklist.save(session_layout.blacklist_file_path)
 
     return 0
 
-# Example: python3 photometry.py -O RR_Lyr -t 20230704 -w /home/user/work -i /home/user/img
+# Example: python3 measure_images.py -O RR_Lyr -t 20230704 -w /home/user/work -i /home/user/img
 
 if __name__ == '__main__':
     sys.exit(main())
