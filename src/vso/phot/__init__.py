616cf4dfafe943f500dd701bdb2629d6d0d51412
dmitrymu <d.v.mukhortov@gmail.com>
2025-03-22 20:54:39 -0700

    Remove obsolete code

--- src/vso/phot/__init__.py
+++ src/vso/phot/__init__.py
@@ -1,7 +1,3 @@
-from .BatchTransformer import BatchTransformer
 from .BatchDataProvider import BatchDataProvider
-from .BulkPhotometry import BulkPhotometry
-from .DataProvider import DataProvider, Filter
 from .measure import *
-from .SimpleTransform import SimpleTransform
 from .transform import *
