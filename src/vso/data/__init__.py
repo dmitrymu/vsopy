3924fa2aa8ed54d3abdaffe742569cdcccd42720
Dmitry M <d.v.mukhortov@gmail.com>
2025-07-27 14:43:51 -0700

    remove package sources

--- src/vso/data/__init__.py
+++
@@ -1,6 +0,0 @@
-from .AavsoApi import AavsoApi
-from .AavsoParser import AavsoParser
-from .AavsoReport import AavsoReport
-from .PersistentTable import PersistentTable
-from .StarData import StarData
-from .CameraRegistry import CameraRegistry
